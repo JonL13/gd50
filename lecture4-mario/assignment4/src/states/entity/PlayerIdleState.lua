8a177a9037c99661b5c537351d977702e3fb4761
jonl13 <jonlamont13@gmail.com>
2020-05-03 16:46:31 -0400

    Assignment4: Spawn is always above ground (and a few Linux fixes)

--- lecture4-mario/assignment4/src/states/entity/PlayerIdleState.lua
+++ lecture4-mario/assignment4/src/states/entity/PlayerIdleState.lua
@@ -24,7 +24,7 @@ function PlayerIdleState:update(dt)
         self.player:changeState('walking')
     end
 
-    if love.keyboard.wasPressed('space') then
+    if love.keyboard.wasPressed('space') or love.keyboard.wasPressed(' ') then
         self.player:changeState('jump')
     end
 
--- lecture4-mario/assignment4/src/states/entity/PlayerWalkingState.lua
+++ lecture4-mario/assignment4/src/states/entity/PlayerWalkingState.lua
@@ -57,7 +57,7 @@ function PlayerWalkingState:update(dt)
         end
     end
 
-    if love.keyboard.wasPressed('space') then
+    if love.keyboard.wasPressed('space') or love.keyboard.wasPressed(' ') then
         self.player:changeState('jump')
     end
 end
