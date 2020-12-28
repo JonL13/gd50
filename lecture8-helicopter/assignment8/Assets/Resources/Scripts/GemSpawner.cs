321d10e95ee5bf8032b34c2192c725dc376de8ef
Jon Lamont <JonLamont13@Gmail.com>
2020-12-28 18:02:56 -0500

    Assignment 8: adding gems and resetting speed when resetting game

---
+++ lecture8-helicopter/assignment8/Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // infinite gem spawning function, asynchronous
+		StartCoroutine(SpawnGems());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnGems(){
+        while(true){
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+        
+            // pause 1-5 seconds until the next coin spawns
+            yield return new WaitForSeconds(Random.Range(3, 8));
+        }
+
+    }
+}
